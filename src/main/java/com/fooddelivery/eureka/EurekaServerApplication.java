927e89b4b0fa45dc117da476bf5496baaa6fad21
Parthu <parthudhanireddy@gmail.com>
2026-09-06 18:25:37 +0530

    fix: exclude DataSourceAutoConfiguration in tests

--- src/main/java/com/fooddelivery/eureka/EurekaServerApplication.java
+++ src/main/java/com/fooddelivery/eureka/EurekaServerApplication.java
@@ -4,10 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@SpringBootApplication(
-    scanBasePackages = {"com.fooddelivery.eureka", "com.fooddelivery.common"}
-)
-@org.springframework.context.annotation.ComponentScan({"com.fooddelivery", "com.fooddelivery.common"})
+@SpringBootApplication
+
 @EnableEurekaServer
 public class EurekaServerApplication {
     public static void main(String[] args) {
--- src/test/java/com/fooddelivery/eureka/EurekaServerApplicationStartupTest.java
+++ src/test/java/com/fooddelivery/eureka/EurekaServerApplicationStartupTest.java
@@ -3,7 +3,10 @@ package com.fooddelivery.eureka;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, properties = {"spring.cloud.config.enabled=false"})
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, properties = {
+    "spring.cloud.config.enabled=false",
+    "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration,org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration,org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration"
+})
 class EurekaServerApplicationStartupTest {
 @Test
     void contextLoads() {
